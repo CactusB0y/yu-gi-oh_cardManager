eb7c4d9fd6696e1d2b4ff3bfd365701d1441f178
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2022-01-27 11:07:15 +0100

    Navbar created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="bg-white shadow dark:bg-gray-800">
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <router-link to="/" href="#" class="text-gray-800 transition-colors duration-200 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">HOME</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
